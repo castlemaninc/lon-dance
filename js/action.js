9b23264829258523129db98458bd44e169d21a8a
Bryan Castleman <castlemaninc@gmail.com>
2018-05-10 12:01:33 -0700

    changed small navigation to slide down and up on click and replaced missing jquery

--- js/action.js
+++ js/action.js
@@ -1,14 +1,40 @@
 $(document).ready(function(){
-	console.log(" I'm working! ");
+	
+	console.log('ready');
+
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 0){
+			$('.main-header').css('box-shadow', '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)');
+		} else {
+			$('.main-header').css('box-shadow', 'none');
+		}
+	});
+
+	$('.main-nav a').click(function(e){
+		console.log('clicked');
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop: $(this.hash).offset().top -130
+		}, 500);		
+	});
+
+	$('.hero-content a').click(function(e){
+		console.log('clicked');
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop: $(this.hash).offset().top -130
+		}, 500);		
+	});
 
 	$('.bars').on('click', function(){
-		$('.small-device-nav-links').css('display', 'block');
+		$('.small-device-nav-links').slideDown(500);
 		$('.bars').hide();
 		$('.close-x').show();
+
 	});
 
 	$('.close-x').on('click', function(){
-		$('.small-device-nav-links').css('display', 'none');
+		$('.small-device-nav-links').slideUp(500);
 		$('.bars').show();
 		$('.close-x').hide();
 	});
